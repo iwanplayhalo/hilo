a49ff7a3a7f55faf7d5553c88f0e0a79c4f3aff8
Daniel Pak <pakdaniel563@gmail.com>
2026-03-03 23:33:33 -0500

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -15,12 +15,12 @@ export default function App({ Component, pageProps }) {
   return (
     <>
         <Head>
-          <title>MVP Starter</title>
-          <meta name='description' content='Put a description here about your app'/>
+          <title>Hi Lo!</title>
+          <meta name='description' content='Simple game where we guess if the right card percentage is higher or lower than the left'/>
           <meta name='robots' content='index, follow'/>
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon_package/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon_package/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon_package/favicon-16x16.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/hilo.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/hilo.png"/>
           <link rel="manifest" href="/favicon_package/site.webmanifest"/>
           <meta name="msapplication-TileColor" content="#da532c"/>
           <meta name="theme-color" content="#ffffff"/>
